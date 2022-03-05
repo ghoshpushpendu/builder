a6ca59a1c4f14a4478785d636c35058eef449812
Pushpendu Ghosh <pushpendu.ghosh@mefy.care>
2022-03-05 10:50:45 +0530

    chnages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "69e0ee50ec16e27d"; }
+/******/ 		__webpack_require__.h = function() { return "c3f15245e0432f27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

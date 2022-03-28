d1657f2e4c9dc4ac264a47c214b42f58e0b27429
Pushpendu Ghosh <pushpendu.ghosh@mefy.care>
2022-03-28 08:51:03 +0530

    chnages

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
 		mongouri : "mongodb+srv://pushpendu:thesigmapassword@thesigma-mongodb-develo.ykgql.mongodb.net/thesigma?retryWrites=true&w=majority",
 	    google_client_id:"259083905478-9vs3cimdg8n1ktrdla6cu5iavp3879qd.apps.googleusercontent.com",
 	    google_client_secret: "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"
-	}
+	},
+	distDir:"build"
 }

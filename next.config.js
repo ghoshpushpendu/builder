7757b149763822e98dbc4750f2db5049a84b8c50
Pushpendu Ghosh <pushpendu.ghosh@mefy.care>
2022-03-29 06:47:32 +0530

    chnages

--- next.config.js
+++ next.config.js
@@ -4,5 +4,4 @@ module.exports = {
 	    google_client_id:"259083905478-9vs3cimdg8n1ktrdla6cu5iavp3879qd.apps.googleusercontent.com",
 	    google_client_secret: "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"
 	},
-	distDir:"build",
 }
